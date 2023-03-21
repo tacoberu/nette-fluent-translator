47a92c9c1accb71dba2d01b4a9904e373c4ec906
Martin Takáč <martin@takac.name>
2023-03-21 03:24:31 +0100

    add Translator method: changeLocaleTo()

--- libs/Translator.php
+++ libs/Translator.php
@@ -15,6 +15,7 @@ class Translator implements ITranslator
 	private $loader;
 	private $localeResolver;
 	private $default;
+	private $currentLocale;
 	private $supported;
 
 	function __construct(LocaleResolver $localeResolver, MessageLoader $loader, $default, array $supported)
@@ -41,11 +42,33 @@ class Translator implements ITranslator
 
 
 
+	function changeLocaleTo(string $code)
+	{
+		// @TODO Validace
+		$this->currentLocale = $code;
+		$this->catalog = Null;
+	}
+
+
+
 	private function getCatalog()
 	{
 		if (empty($this->catalog)) {
-			$this->catalog = $this->loader->loadFor($this->localeResolver->resolve($this->supported, $this->default));
+			$this->catalog = $this->loader->loadFor($this->resolveLocaleCode());
 		}
 		return $this->catalog;
+	}
+
+
+
+	/**
+	 * Vybere kód. Buď je explicintě určen, nebo se automagicky odvodí na základě prostředí.
+	 */
+	private function resolveLocaleCode() : string
+	{
+		if ($this->currentLocale) {
+			return $this->currentLocale;
+		}
+		return $this->localeResolver->resolve($this->supported, $this->default);
 	}
 }
